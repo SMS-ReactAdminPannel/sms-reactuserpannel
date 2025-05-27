392e055e3572f3a970dbde1e5ae5dbcbb93cea3c
Abishekyoho <abish.yoho@gmail.com>
2025-05-27 18:48:34 +0530

    add scroll bar

--- src/pages/Bookings/BookingsPage.tsx
+++ src/pages/Bookings/BookingsPage.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
-const BookingsPage = () => {
-	return <div>BookingsPage</div>;
+import AlternatingProductCardList from '../../components/bookings/BookingCard/BookingCard';
+import ScrollableNavbar from '../../components/bookings/BookingScroll/BookingScroll';
+
+const services = [
+  "Oil Change", "Tire Rotation", "Brake Inspection", "Battery Check",
+  "Engine Diagnostics", "AC Service", "Wheel Alignment", "Car Wash",
+  "Headlight Check", "Transmission Flush"
+];
+
+const BookingsPage: React.FC = () => {
+  return (
+    <div className="p-4 ">
+      
+      <div className='mb-2'>
+        <ScrollableNavbar items={services} />
+      </div>
+
+      {/* <div className='grid grid-cols-'>
+        <AlternatingProductCardList/>
+      </div> */}
+    </div>
+  );
 };
 
 export default BookingsPage;
