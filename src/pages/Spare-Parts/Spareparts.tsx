38250916ebff19fcf1403548d78e8a205247a69a
suryas-1805 <suryas18051998@gmail.com>
2025-07-16 12:56:23 +0530

    changes

--- src/pages/Spare-Parts/Spareparts.tsx
+++ src/pages/Spare-Parts/Spareparts.tsx
@@ -327,22 +327,24 @@ const SpareParts: React.FC = () => {
 										</div>
 										<div
 											className={`mt-2 text-xs font-semibold ${
-												part?.inStock ? 'text-green-600' : 'text-red-600'
+												part?.stock >= 1 ? 'text-green-600' : 'text-red-600'
 											}`}
 										>
-											{part?.inStock ? 'In Stock' : 'Out of Stock'}
+											{part?.stock >= 1 ? 'In Stock' : 'Out of Stock'}
 										</div>
 
 										{/* Cart Icon Button */}
-										<button
-											className='absolute bottom-4 right-5 bg-white p-3 rounded-full shadow hover:bg-[#0050A5] hover:text-white transition'
-											onClick={(e) => {
-												e.stopPropagation();
-												handleAddToCart(part);
-											}}
-										>
-											<ShoppingCart size={28} />
-										</button>
+										{part?.stock >= 1 && (
+											<button
+												className='absolute bottom-4 right-5 bg-white p-3 rounded-full shadow hover:bg-[#0050A5] hover:text-white transition'
+												onClick={(e) => {
+													e.stopPropagation();
+													handleAddToCart(part);
+												}}
+											>
+												<ShoppingCart size={28} />
+											</button>
+										)}
 									</div>
 								</div>
 							))}
