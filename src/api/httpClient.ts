0cd0aab878948144e2c2205fb165e0c5221dae64
mugilane.cse <55045435+Mugilane@users.noreply.github.com>
2025-07-16 09:39:42 +0530

    update

--- src/api/httpClient.ts
+++ src/api/httpClient.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const backEndUrl: string = 'https://sms-node-backend-17xb.onrender.com';
-// const backEndUrl: string = 'http://192.168.1.18:3000';
+// const backEndUrl: string = 'http://localhost:3000';
 
 const Axios = axios.create({
 	baseURL: backEndUrl,
